5aad46a0ef8ddacfd2b35d93c6e478d4880c5e03
ArmedBugaabooz <54632618+ArmedBugaabooz@users.noreply.github.com>
2020-08-15 01:43:51 +0530

    fully functional

--- script.js
+++ script.js
@@ -1,32 +1,82 @@
+var output;
+var inputData;
+var companies = [];
+const roles = ["Frontend", "Backend", "Fullstack"];
+var dataRole = null;
+const levels = ["Junior", "Midweight", "Senior"];
+var dataLevel = null;
+const languages = ["Python", "Ruby", "JavaScript", "HTML", "CSS"];
+var dataLanguage = null;
+const tools = ["React", "Sass", "Vue", "Django", "RoR"];
+var dataTool = null;
 var input = document.querySelector('input');
 var list = document.querySelector('.output');
-var lis;
+
+fetcher();
 
 input.addEventListener('keypress', checkFunction);
 
-function checkFunction(event) {
-  if(event.keyCode == '13') {
-    fetch('data.json')
+function fetcher() {
+  fetch('data.json')
     .then((res) => res.json())
     .then((data) => {
-      var companies = data;
-      var output = '';
-      companies.forEach(company => {
-        var categories = [company.languages, company.tools, company.role, company.level];
-        if (categories[0].includes(input.value) || categories[1].includes(input.value) || categories[2] == input.value || categories[3] == input.value) {
-          for(var i = 0 ; i < categories.length ; i++){
-            if(categories[i] == ""){
-              categories.splice(i,1);
-            }
-          }
-          output += `<ul>`
-          categories.forEach(category => {
-            output += `<li>${category}</li>`            
-          });
-          output += `</ul>`;
-        }
-      })
-      list.innerHTML = output;
+      companies = data;
     })
+}
+
+function checkFunction(event) {
+  if (event.keyCode == '13')checker();
+}  
+
+function checker() {
+  inputData = input.value.split(" ");
+  undefiner();
+  arrayAssigner();
+  // console.log(inputData);
+  output = '';
+  companies.forEach(company => {
+    // console.log(dataLanguage, dataRole, dataTool, dataLevel);
+    var categories = [company.languages, company.tools, company.role, company.level];
+    if ((categories[0].includes(dataLanguage) || dataLanguage == null) && 
+    (categories[1].includes(dataTool) || dataTool == null) && 
+      (categories[2] == dataRole || dataRole == null) && 
+      (categories[3] == dataLevel || dataLevel == null)) {
+      for (var i = 0; i < categories.length; i++) {
+        if (categories[i] == "") {
+          categories.splice(i, 1);
+        }
+      }
+      output += `<ul>`
+      categories.forEach(category => {
+        output += `<li>${category}</li>`
+      });
+      output += `</ul>`;
+    }
+  })
+  list.innerHTML = output;
+}
+
+function undefiner() {
+  dataRole = null;
+  dataLevel = null;
+  dataLanguage = null;
+  dataTool = null;
+}
+
+function arrayAssigner() {
+  for (var i = 0 ; i < inputData.length ; i++){
+    if (roles.includes(inputData[i]) && dataRole == null) {
+      dataRole = inputData[i];
+      continue;
+    } else if (levels.includes(inputData[i]) && dataLevel == null) {
+      dataLevel = inputData[i];
+      continue;
+    } else if (languages.includes(inputData[i]) && dataLanguage == null) {
+      dataLanguage = inputData[i];
+      continue;
+    } else if (tools.includes(inputData[i]) && dataLevel == null) {
+      dataTool = inputData[i];
+      continue;
+    }
   }
 }
