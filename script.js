ffdbd5bfb37878016bea7393913e1da4b705812b
ArmedBugaabooz <54632618+ArmedBugaabooz@users.noreply.github.com>
2020-08-15 07:11:30 +0530

    fully functional and styled

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var output;
+var output = '';
 var inputData;
 var companies = [];
 const roles = ["Frontend", "Backend", "Fullstack"];
@@ -12,17 +12,13 @@ var dataTool = null;
 var input = document.querySelector('input');
 var list = document.querySelector('.output');
 
-fetcher();
-
+fetch('data.json')
+  .then((res) => res.json())
+  .then((data) => {
+    companies = data;
+  })
+  
 input.addEventListener('keypress', checkFunction);
-
-function fetcher() {
-  fetch('data.json')
-    .then((res) => res.json())
-    .then((data) => {
-      companies = data;
-    })
-}
 
 function checkFunction(event) {
   if (event.keyCode == '13')checker();
@@ -37,21 +33,22 @@ function checker() {
   companies.forEach(company => {
     // console.log(dataLanguage, dataRole, dataTool, dataLevel);
     var categories = [company.languages, company.tools, company.role, company.level];
-    if ((categories[0].includes(dataLanguage) || dataLanguage == null) && 
-    (categories[1].includes(dataTool) || dataTool == null) && 
-      (categories[2] == dataRole || dataRole == null) && 
-      (categories[3] == dataLevel || dataLevel == null)) {
-      for (var i = 0; i < categories.length; i++) {
-        if (categories[i] == "") {
-          categories.splice(i, 1);
+    if (dataLanguage == null && dataLevel == null && dataRole == null && dataTool == null) {
+      outputer(company);
+    } else {
+      if ((categories[0].includes(dataLanguage) || dataLanguage == null) &&
+        (categories[1].includes(dataTool) || dataTool == null) &&
+        (categories[2] == dataRole || dataRole == null) &&
+        (categories[3] == dataLevel || dataLevel == null)) {
+        for (var i = 0; i < categories.length; i++) {
+          if (categories[i] == "") {
+            categories.splice(i, 1);
+          }
         }
+        outputer(company);
       }
-      output += `<ul>`
-      categories.forEach(category => {
-        output += `<li>${category}</li>`
-      });
-      output += `</ul>`;
     }
+    
   })
   list.innerHTML = output;
 }
@@ -67,16 +64,40 @@ function arrayAssigner() {
   for (var i = 0 ; i < inputData.length ; i++){
     if (roles.includes(inputData[i]) && dataRole == null) {
       dataRole = inputData[i];
-      continue;
     } else if (levels.includes(inputData[i]) && dataLevel == null) {
       dataLevel = inputData[i];
-      continue;
     } else if (languages.includes(inputData[i]) && dataLanguage == null) {
       dataLanguage = inputData[i];
-      continue;
-    } else if (tools.includes(inputData[i]) && dataLevel == null) {
+    } else if (tools.includes(inputData[i]) && dataTool == null) {
       dataTool = inputData[i];
-      continue;
     }
   }
+}
+
+function outputer(company) {
+  output += `
+      <div class="company">
+        <img src="${company.logo}" alt="">
+        <p class="heading">${company.company}</p>
+        `
+  if (company.new) output += `<p class="new">New!</p>`;
+  if (company.featured) output += `<p class="featured">Featured!</p>`
+  output += `
+        <br>
+        <p class="position">${company.position}</p>
+        <div class="roles">
+          <p>${company.role}</p>
+          <p>${company.level}</p>`
+  company.languages.forEach(language => {
+    output += `<p>${language}</p>`
+  });
+  company.tools.forEach(tool => {
+    output += `<p>${tool}</p>`
+  })
+  output += ` </div>
+        <div class="info">
+          <p>${company.postedAt}  .  ${company.contract}  .  ${company.location}</p>
+        </div>
+      </div>
+      `;
 }
